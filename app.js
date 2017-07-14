e2bb3c6935b00022bff38e6e3272b01c35e81ffd
Will Gant <w_gant@yahoo.co.uk>
2017-07-14 14:53:54 +0000

    Demo of another way to export functions

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('./instantHello');
 var goodbye = require('./talk/goodbye');
 var talk = require('./talk');
+var question = require('./talk/question');
 
 talk.intro();
 talk.hello("Will")
+
+var answer = question.ask("What's the meaning of life?")
+console.log(answer)
 
 goodbye();
