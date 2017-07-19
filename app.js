503a7d1f5118f8099e19399e5db3b807b00b34de
Will Gant <w_gant@yahoo.co.uk>
2017-07-19 10:11:23 +0000

    Changes middleware to apply only to certain requests

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.set('port', 8080);
 
-app.use(function(req, res, next) {
+app.use('/css', function(req, res, next) {
     console.log(req.method, req.url);
     next();
 });
