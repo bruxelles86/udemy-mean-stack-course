ed007d0f1fcb676527ddc9f02dc81215836c77e3
Will Gant <w_gant@yahoo.co.uk>
2017-07-20 11:30:28 +0000

    Gets single doc based on ID parameter given in url

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,54 +1,50 @@
 var dbconn = require('../data/dbconnection.js');
 var hotelData = require('../data/hotel-data.json');
+var ObjectId = require('mongodb').ObjectId;
 
 module.exports.hotelsGetAll = function(req, res) {
     
     var db = dbconn.get();
     var collection = db.collection('hotels');
     
+    var offset = 0;
+    var count = 5;
+    
+    if (req.query && req.query.offset) {
+        offset = parseInt(req.query.offset, 10);
+    }
+    
+    if (req.query && req.query.count) {
+        count = parseInt(req.query.count, 10);
+    }
+    
     collection
         .find()
+        .skip(offset)
+        .limit(count)
         .toArray(function(err, docs) {
             console.log("Found hotels", docs);
             res
                 .status(200)
                 .json(docs);
         });
-    
-    
-    // console.log("db", db);
-    
-    // console.log("GET the json");
-    // console.log(req.query)
-    
-    // var offset = 0;
-    // var count = 5;
-    
-    // if (req.query && req.query.offset) {
-    //     offset = parseInt(req.query.offset, 10);
-    // }
-    
-    // if (req.query && req.query.count) {
-    //     count = parseInt(req.query.count, 10);
-    // }
-    
-    // var returnData = hotelData.slice(offset,offset+count)
-    
-    // res  
-    //     .status(200)
-    //     .json( returnData );
 };
 
 module.exports.hotelsGetOne = function(req, res) {
     var db = dbconn.get();
-    console.log("db", db);
+    var collection = db.collection('hotels');
     
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId];
     console.log("GET hotelId", hotelId);
-    res  
-        .status(200)
-        .json( thisHotel );
+    
+    collection
+        .findOne({ 
+            _id : ObjectId(hotelId)
+        }, function(err, doc) {
+            res  
+              .status(200)
+              .json( doc );
+        });
 };
 
 module.exports.hotelsAddOne = function(req, res) {
